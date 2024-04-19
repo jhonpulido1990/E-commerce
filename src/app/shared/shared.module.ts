ab79783c6417a4c0c19a2a2c5494108e6fa14880
jhon-puli <3825@holbertonstudents.com>
2024-04-19 02:20:44 -0500

    banner2

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from './card/card.component';
+import { Banner2Component } from './banner2/banner2.component';
 
 
 
 @NgModule({
   declarations: [
-    CardComponent
+    CardComponent,
+    Banner2Component
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    CardComponent
+    CardComponent,
+    Banner2Component,
   ]
 })
 export class SharedModule { }
