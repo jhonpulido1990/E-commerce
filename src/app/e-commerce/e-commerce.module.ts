ea4b5cc926509e886471ea29eeb547c064b25e64
jhon-puli <3825@holbertonstudents.com>
2024-04-19 00:45:09 -0500

    card header

--- src/app/e-commerce/e-commerce.module.ts
+++ src/app/e-commerce/e-commerce.module.ts
@@ -5,6 +5,7 @@ import { ECommerceRoutingModule } from './e-commerce-routing.module';
 import { HomeComponent } from './pages/home/home.component';
 import { ProductComponent } from './pages/product/product.component';
 import { LayoutModule } from './layout/layout.module';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
@@ -16,6 +17,7 @@ import { LayoutModule } from './layout/layout.module';
     CommonModule,
     ECommerceRoutingModule,
     LayoutModule,
+    SharedModule,
   ]
 })
 export class ECommerceModule { }
