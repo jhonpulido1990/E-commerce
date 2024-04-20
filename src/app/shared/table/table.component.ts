e1bdedced87efac6e9e60bb746c532cc5bfa3649
jhon-puli <3825@holbertonstudents.com>
2024-04-20 00:16:30 -0500

    pruebas de consulta

--- src/app/shared/table/table.component.ts
+++ src/app/shared/table/table.component.ts
@@ -1,4 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Product } from '../../e-commerce/interfaces/product.interfaces';
 
 @Component({
   selector: 'app-table',
@@ -6,5 +9,11 @@ import { Component, signal } from '@angular/core';
   styleUrl: './table.component.scss'
 })
 export class TableComponent {
-  public products = signal([])
+  item$: Observable<Product[]>;
+  firestore: Firestore = inject(Firestore);
+
+  constructor() {
+    const itemCollection = collection(this.firestore, 'ListProduct');
+    this.item$ = collectionData(itemCollection) as Observable<Product[]>;
+  }
 }
